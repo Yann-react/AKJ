1ffc5d4535a717ea0fc9fe161dcff52115fd20d7
Yann-react <blamusjunior@gmail.com>
2022-07-20 12:07:06 +0100

    c

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import WelcomePage from './components/WelcomePage';
 import Login from './components/Login';
 import LoginSuite from './components/LoginSuite';
 import SignIn from './components/SignIn';
-import WalletClient from './components/WalletClient';
+import WalletClient from './components/WalletCLient';
 import DetailComponent from './components/DetailComponent';
 import WalletLivreur from './components/WalletLivreur';
 import MenuLivreur from './components/MenuLivreur';
